565cb6e5da329f27a40f7f8b61d25d1a9c467863
kennedywc <kennedy.costan10@gmail.com>
2024-07-09 22:30:54 -0300

    ♻️ Refactoring for gateway regarding post routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 import { GatewayModule } from './gateway/gateway.module';
 import { ConfigModule } from '@nestjs/config';
-import { PostsModule } from './posts/posts/posts.module';
-import { CategoriesModule } from './posts/categories/categories.module';
-import { VotesModule } from './posts/votes/votes.module';
-
 
 @Module({
-  imports: [ConfigModule.forRoot(), GatewayModule, PostsModule, CategoriesModule, VotesModule],
+  imports: [ConfigModule.forRoot(), GatewayModule],
   controllers: [],
 })
 export class AppModule {}
